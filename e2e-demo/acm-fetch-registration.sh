369c5438e669dc01a642b2ec77b67ef7c32ec910
jcope <jcope@redhat.com>
2021-07-23 17:05:59 -0500

    move scripts to e2e-demo

--- e2e-demo/acm-fetch-registration.sh
+++ e2e-demo/acm-fetch-registration.sh
@@ -2,7 +2,6 @@
 
 set -eu
 
-
 CLUSTER_NAME=${1:?"expected the cluster name, required to find import data"}
 BUCKET=acm-microshift-demo
 aws s3 cp --recursive s3://$BUCKET/$CLUSTER_NAME /var/lib/microshift/manifests
