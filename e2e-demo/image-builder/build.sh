fbf6becef9b4e4764a024b91c16bafce516a14a5
Frank A. Zdarsky <fzdarsky@redhat.com>
2021-07-29 10:50:16 +0200

    Update prepare and build scripts
    Signed-off-by: Frank A. Zdarsky <fzdarsky@redhat.com>

--- e2e-demo/image-builder/build.sh
+++ e2e-demo/image-builder/build.sh
@@ -1,26 +1,70 @@
 #!/bin/bash
 
-set -exo pipefail
+set -e -o pipefail
 
-REPOROOT=$(git rev-parse --show-toplevel)
+DEMOROOT=$(git rev-parse --show-toplevel)/e2e-demo
 
-sudo composer-cli blueprints delete r4e-microshift 2>/dev/null || true
-sudo composer-cli blueprints push ./r4e-microshift.toml
-sudo composer-cli blueprints depsolve r4e-microshift
+title() {
+    echo -e "\E[34m\n# $1\E[00m";
+}
 
-UUID=$(sudo composer-cli compose start r4e-microshift rhel-edge-commit | awk '{print $2}')
+load_blueprint() {
+    sudo composer-cli blueprints delete $1 2>/dev/null || true
+    sudo composer-cli blueprints push ${DEMOROOT}/image-builder/$1.toml
+    sudo composer-cli blueprints depsolve $1
+}  
 
-STATUS=$(sudo composer-cli compose status | grep ${UUID} | awk '{print $2}')
-while [ ${STATUS} !=  FINISHED ]; do
-    sleep 60
-    STATUS=$(sudo composer-cli compose status | grep ${UUID} | awk '{print $2}')
-    echo $(date --rfc-3339=seconds -u) ${STATUS}
-done
+waitfor_image() {
+    STATUS=$(sudo composer-cli compose status | grep $1 | awk '{print $2}')
+    while [ ${STATUS} !=  FINISHED ]; do
+        sleep 10
+        STATUS=$(sudo composer-cli compose status | grep $1 | awk '{print $2}')
+        echo $(date +'%Y-%m-%d %H:%M:%S') ${STATUS}
+    done
+}
 
-mkdir -p ${REPOROOT}/builds
-pushd ${REPOROOT}/builds &>/dev/null
-echo ${UUID} > uuid.txt
-sudo composer-cli compose logs ${UUID}
-sudo composer-cli compose image ${UUID}
-sudo composer-cli compose metadata ${UUID}
+download_image() {
+    sudo composer-cli compose logs $1
+    sudo composer-cli compose metadata $1
+    sudo composer-cli compose image $1
+}
+
+
+mkdir -p ${DEMOROOT}/builds
+pushd ${DEMOROOT}/builds &>/dev/null
+
+title "Adding RHOCP and Ansible repos to builder"
+sudo cp ${DEMOROOT}/image-builder/rhel-8.json /etc/osbuild-composer/repositories/
+sudo systemctl restart osbuild-composer.service
+
+title "Loading r4e-microshift blueprint"
+load_blueprint r4e-microshift
+
+title "Building r4e-microshift ostree container image"
+UUID=$(sudo composer-cli compose start-ostree --ref rhel/8/$(uname -i)/edge r4e-microshift rhel-edge-container | awk '{print $2}')
+waitfor_image ${UUID}
+download_image ${UUID}
+
+title "Serving r4e-microshift ostree container locally"
+IMAGEID=$(cat ./${UUID}-rhel84-container.tar | sudo podman load | grep -o -P '(?<=@)[a-z0-9]*')
+sudo podman tag ${IMAGEID} localhost/rhel-edge-container
+sudo podman rm -f rhel-edge-container 2>/dev/null || true
+sudo podman run -d --name=rhel-edge-container -p 8080:80 localhost/rhel-edge-container
+
+title "Removing RHOCP and Ansible repos from builder" # builder trips on it
+sudo rm /etc/osbuild-composer/repositories/rhel-8.json
+sudo systemctl restart osbuild-composer.service
+
+title "Loading installer blueprint"
+load_blueprint installer
+
+title "Building installer ISO"
+UUID=$(sudo composer-cli compose start-ostree --ref rhel/8/$(uname -i)/edge --url http://localhost:8080/repo installer rhel-edge-installer | awk '{print $2}')
+waitfor_image ${UUID}
+download_image ${UUID}
+
+title "Cleaning up local ostree container serving"
+sudo podman rm -f rhel-edge-container 2>/dev/null || true
+sudo podman rmi -f ${IMAGEID} 2>/dev/null || true
+
 popd &>/dev/null
--- e2e-demo/image-builder/prepare_builder.sh
+++ e2e-demo/image-builder/prepare_builder.sh
@@ -2,12 +2,8 @@
 
 set -exo pipefail
 
-# install image builder
-sudo dnf install -y git osbuild-composer composer-cli cockpit-composer bash-completion
+sudo dnf install -y git osbuild-composer composer-cli cockpit-composer bash-completion podman
 sudo systemctl enable osbuild-composer.socket --now
 sudo systemctl enable cockpit.socket --now
 sudo firewall-cmd -q --add-service=cockpit
 sudo firewall-cmd -q --add-service=cockpit --permanent
-
-sudo cp rhel-8.json /usr/share/osbuild-composer/repositories/ && rm rhel-8.json
-sudo systemctl restart osbuild-composer.service
