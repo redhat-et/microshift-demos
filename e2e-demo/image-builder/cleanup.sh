bdb96287338e891dc879b540d59a6bd700fd91d8
Frank A. Zdarsky <fzdarsky@redhat.com>
2021-07-30 19:05:40 +0200

    Add clean up script
    Signed-off-by: Frank A. Zdarsky <fzdarsky@redhat.com>

---
+++ e2e-demo/image-builder/cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+DEMOROOT=$(git rev-parse --show-toplevel)/e2e-demo
+
+sudo rm -f ${DEMOROOT}/builds/*
+
+for uuid in $(sudo composer-cli compose list | awk '{print $1}'); do
+    echo "Deleting compose ${uuid}"
+    sudo composer-cli compose cancel ${uuid}
+    sudo composer-cli compose delete ${uuid}
+done
