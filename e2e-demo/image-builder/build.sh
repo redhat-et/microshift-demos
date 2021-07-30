1387385a67b4bbbf8cb60b41ff27a88954be6f94
Frank A. Zdarsky <fzdarsky@redhat.com>
2021-07-30 19:06:21 +0200

    Embed kickstart into ISO
    Signed-off-by: Frank A. Zdarsky <fzdarsky@redhat.com>

--- e2e-demo/image-builder/build.sh
+++ e2e-demo/image-builder/build.sh
@@ -62,10 +62,16 @@ title "Building installer ISO"
 UUID=$(sudo composer-cli compose start-ostree --ref rhel/8/$(uname -i)/edge --url http://localhost:8080/repo/ installer rhel-edge-installer | awk '{print $2}')
 waitfor_image ${UUID}
 download_image ${UUID}
-cp -f ${UUID}-rhel84-boot.iso r4e-microshift-installer.iso
 
 title "Cleaning up local ostree container serving"
 sudo podman rm -f rhel-edge-container 2>/dev/null || true
 sudo podman rmi -f ${IMAGEID} 2>/dev/null || true
 
+title "Embedding kickstart"
+cp ${DEMOROOT}/image-builder/kickstart.ks ${DEMOROOT}/builds/kickstart.ks
+sudo podman run --rm --privileged -ti -v ${DEMOROOT}/builds:/data -v /dev:/dev fedora /bin/bash -c \
+    "dnf -y install lorax; cd /data; mkksiso kickstart.ks ${UUID}-rhel84-boot.iso r4e-microshift-installer.iso; exit"
+sudo chown $(whoami). ${DEMOROOT}/builds/r4e-microshift-installer.iso
+
+title "Done"
 popd &>/dev/null
--- e2e-demo/image-builder/prepare_builder.sh
+++ e2e-demo/image-builder/prepare_builder.sh
@@ -2,7 +2,7 @@
 
 set -exo pipefail
 
-sudo dnf install -y git osbuild-composer composer-cli cockpit-composer bash-completion podman
+sudo dnf install -y git osbuild-composer composer-cli cockpit-composer bash-completion podman genisoimage syslinux
 sudo systemctl enable osbuild-composer.socket --now
 sudo systemctl enable cockpit.socket --now
 sudo firewall-cmd -q --add-service=cockpit
