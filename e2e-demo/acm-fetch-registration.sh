d4c5140fe560f8be5e2acd8e99e1dea9fb62e259
Frank A. Zdarsky <fzdarsky@redhat.com>
2021-08-16 15:54:51 +0200

    Remove fetching of regsitration from S3
    Signed-off-by: Frank A. Zdarsky <fzdarsky@redhat.com>

--- e2e-demo/acm-fetch-registration.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-set -eu
-
-CLUSTER_NAME=${1:?"expected the cluster name, required to find import data"}
-BUCKET=acm-microshift-demo
-aws s3 cp --recursive s3://$BUCKET/$CLUSTER_NAME /var/lib/microshift/manifests
-
