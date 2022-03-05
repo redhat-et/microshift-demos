09c4dc304ae8788d386c58ba2bcbd31cdecd0ba2
Frank A. Zdarsky <fzdarsky@redhat.com>
2022-03-05 16:17:56 +0100

    Continue when cancel fails
    Signed-off-by: Frank A. Zdarsky <fzdarsky@redhat.com>

--- e2e-demo/image-builder/cleanup.sh
+++ e2e-demo/image-builder/cleanup.sh
@@ -8,6 +8,6 @@ sudo rm -f ${DEMOROOT}/builds/*
 
 for uuid in $(sudo composer-cli compose list | awk '{print $1}'); do
     echo "Deleting compose ${uuid}"
-    sudo composer-cli compose cancel ${uuid}
-    sudo composer-cli compose delete ${uuid}
+    sudo composer-cli compose cancel ${uuid} || true
+    sudo composer-cli compose delete ${uuid} || true
 done
